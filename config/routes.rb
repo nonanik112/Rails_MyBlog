2e367f3e618d1658177a2b655765244ac8ee3ae6
nonanik112 <metehan_1306@hotmail.com>
2021-09-02 22:22:27 +0300

    Rails_MyBlog

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+resources :articles
+root 'pages#home'
+get 'about', to: 'pages#about'
+get 'careers', to: 'pages#careers'
+get 'signup', to: 'users#new'
+resources :users, except: [:new]
+get 'login', to: 'sessions#new'
+post 'login', to: 'sessions#create'
+delete 'logout', to: 'sessions#destroy'
+resources :categories, except: [:destroy]
+end
